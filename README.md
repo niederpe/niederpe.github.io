f6ed8819f420bfe83f005b5102642c969ca9c080
Peter Niedermeier <peter@niedermeier-ed.de>
2016-12-02 18:07:15 +0100

    folder structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Peter Niedermeier - Personal page
+
+## Copyright and License
+
+Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-new-age/blob/gh-pages/LICENSE) license.
---
+++ js/app.js
@@ -0,0 +1,17 @@
+(function($) {
+    "use strict";
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+})(jQuery);
+
+$(window).on("load", function() {
+    $(window).scrollTop(0);
+});
